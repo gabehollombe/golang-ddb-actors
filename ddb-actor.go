68aea5f3cc2047e58acd6f58ffaea545911fae8e
peloriante <peloriante@protonmail.com>
2023-11-21 20:38:18 +0800

    in memory repo

--- ddb-actor.go
+++ ddb-actor.go
@@ -8,8 +8,15 @@ type ActorRepository struct {
 	Records map[ActorID]Actor
 }
 
+func NewActorRepository() ActorRepository {
+	return ActorRepository{
+		Records: make(map[string]Actor),
+	}
+}
+
 func (r *ActorRepository) save(a Actor) {
 	r.Records[a.ID] = a
+	fmt.Printf("Repo saved: %+v \n", a)
 }
 func (r *ActorRepository) get(id ActorID) Actor {
 	return r.Records[id]
@@ -25,8 +32,8 @@ type Actor struct {
 	ProcessFunc ProcessFunc
 }
 
-func NewActor(id ActorID, processFn ProcessFunc, state map[string]interface{}) *Actor {
-	return &Actor{
+func NewActor(id ActorID, processFn ProcessFunc, state map[string]interface{}) Actor {
+	return Actor{
 		ID:          id,
 		Inbox:       make([]Message, 0),
 		ProcessFunc: processFn,
@@ -40,10 +47,14 @@ func (a *Actor) addMessage(m Message) {
 
 func (a *Actor) processInbox() []Message {
 	outs := make([]Message, 0)
+
+	// Process all messages in inbox
 	for _, m := range a.Inbox {
 		res := a.ProcessFunc(a, m)
 		outs = append(outs, res...)
 	}
+
+	// Return any messages to send out
 	return outs
 }
 
@@ -55,9 +66,26 @@ func main() {
 		return []Message{}
 	}
 
+	// New Actor
 	a := NewActor("a", messageCountFunc, map[string]interface{}{"count": 0})
+
+	// Add actor to repo
+	repo := NewActorRepository()
+	repo.save(a)
+
+	// Send some messages
 	a.addMessage("hello")
 	a.addMessage("goodbye")
-	outs := a.processInbox()
-	fmt.Printf("Actor state: %v, Outs: %v", a.State, outs)
+	repo.save(a)
+
+	// Ask the actor to do some work
+
+	a.processInbox()
+
+	// Persist the new state of the actor along with the handle messages removed (TODO and eventually the OUT messages)
+	a.Inbox = nil
+	repo.save(a)
+
+	// fmt.Printf("Actor state: %+v, Outs: %+v \n", a.State, outs)
+	// fmt.Printf("Repo: %+v \n", repo)
 }
